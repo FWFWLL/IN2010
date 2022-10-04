55ee4271a24fbe6523b247896e8040430c6baf7c
FWFWLL <ffl@vivaldi.net>
2022-10-05 00:07:05 +0200

    Add implementation of bubble sort

---
+++ Assignment-2/src/BubbleSort.java
@@ -0,0 +1,8 @@
+public final class BubbleSort extends SortingAlgorithm {
+	public static void sort(int[] array) {
+		for(int i = 0; i < array.length - 1; i++)
+			for(int j = 0; j < array.length - 1; j++)
+				if(array[j] > array[j + 1])
+					swap(array, j, j + 1);
+	}
+}
