5c84e79f96f153227506d9ad48a6f74a99e44038
FWFWLL <ffl@vivaldi.net>
2022-10-27 13:04:32 +0200

    Finish exercise 3 of assignment 3

--- Assignment-3/src/Main.java
+++ Assignment-3/src/Main.java
@@ -272,18 +272,119 @@ public class Main {
 		return results;
 	}
 
-	// Exercise 2 driver code
-	private static void exercise2(Map<Actor, List<Actor>> graph) {
-		System.out.println("\nExercise " + NUMBERS + "2" + RESET + ":");
-		printShortestPath(graph, "nm2255973", "nm0000460");
-		printShortestPath(graph, "nm0424060", "nm0000243");
-		printShortestPath(graph, "nm4689420", "nm0000365");
-		printShortestPath(graph, "nm0000288", "nm0001401");
-		printShortestPath(graph, "nm0031483", "nm0931324");
+	// Prints the shortest, weighted path between two Actors
+	private static void printShortestPathDijkstra(Map<Actor, List<Actor>> graph, String srcActorId, String dstActorId) {
+		Actor srcActor = actors.get(srcActorId);
+		Actor dstActor = actors.get(dstActorId);
+
+		Map<Actor, Actor> predecessors = new HashMap<>();
+
+		float totalDistance = dijkstra(graph, srcActor, dstActor, predecessors);
+
+		Stack<Actor> path = new Stack<>();
+		Actor crawl = dstActor;
+		path.push(crawl);
+		while(predecessors.get(crawl) != null) {
+			path.push(predecessors.get(crawl));
+			crawl = predecessors.get(crawl);
+		}
+
+		Actor prev = path.pop();
+		System.out.println("\n" + prev.getName());
+
+		while(!path.empty()) {
+			Actor curr = path.pop();
+
+			Movie commonMovie = findCommonMovies(prev, curr)
+				.stream()
+				.filter(Objects::nonNull)
+				.max(Comparator.comparing(Movie::getRating))
+				.orElseThrow(NoSuchElementException::new);
+
+			System.out.printf("====[ %s (%s%.1f%s) ]===> %s\n", commonMovie.getTitle(), NUMBERS, commonMovie.getRating(), RESET, curr.getName());
+
+			prev = curr;
+		}
+
+		System.out.printf("Total weight: %s%.1f%s\n", NUMBERS, totalDistance, RESET);
 	}
 
-	// Exercise 3 driver code
-	private static void exercise3(Map<Actor, List<Actor>> graph) {
-		System.out.println("\nExercise " + NUMBERS + "3" + RESET + ":\n");
+	// BFS but harder, an implementation of lazy dijkstra's algorithm
+	private static float dijkstra(Map<Actor, List<Actor>> graph, Actor srcActor, Actor dstActor, Map<Actor, Actor> predecessors) {
+		class Pair implements Comparable<Pair> {
+			private Actor key;
+			private Float value;
+				
+			public Pair(Actor key, Float value) {
+				this.key = key;
+				this.value = value;
+			}
+
+			public Actor getKey() {return key;}
+			public Float getValue() {return value;}
+
+			@Override
+			public int compareTo(Pair other) {
+				return this.getValue().compareTo(other.getValue());
+			}
+		}
+
+		Map<Actor, Boolean> visited = new HashMap<>();
+		Map<Actor, Float> distances = new HashMap<>();
+
+		for(Actor actor : graph.keySet()) {
+			visited.put(actor, false);
+			predecessors.put(actor, null);
+			distances.put(actor, MAX_RATING);
+		}
+
+		distances.put(srcActor, 0.0f);
+
+		PriorityQueue<Pair> priorityQueue = new PriorityQueue<>();
+		priorityQueue.offer(new Pair(srcActor, 0.0f));
+
+		while(!priorityQueue.isEmpty()) {
+			Pair entry = priorityQueue.poll();
+
+			Actor index = entry.getKey();
+			float minDistance = entry.getValue();
+
+			visited.put(index, true);
+
+			if(distances.get(index) < minDistance)
+				continue;
+
+			for(Actor curr : graph.get(index)) {
+				if(visited.get(curr))
+					continue;
+
+				float newDistance = distances.get(index) + calculateMinDistance(index, curr);
+
+				if(newDistance < distances.get(curr)) {
+					predecessors.put(curr, index);
+					distances.put(curr, newDistance);
+					priorityQueue.offer(new Pair(curr, newDistance));
+				}
+			}
+
+			if(index == dstActor)
+				return distances.get(dstActor);
+		}
+
+		return MAX_RATING;
+	}
+
+	private static float calculateMinDistance(Actor actorA, Actor actorB) {
+		float highestRating = 0.0f;
+
+		Set<Movie> commonMovies = findCommonMovies(actorA, actorB);
+
+		for(Movie movie : commonMovies) {
+			if(movie != null && movie.getRating() >= highestRating) {
+				highestRating = movie.getRating();
+			}
+		}
+
+		return MAX_RATING - highestRating;
 	}
 }
